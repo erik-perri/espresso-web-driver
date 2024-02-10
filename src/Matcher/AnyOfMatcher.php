4d35a05b14bc2195133610061c2f235f97f89ec0
Erik Perri <erik.perri@gmail.com>
2024-02-09 22:26:28 -0500

    update: Added wait to root of any matcher

--- src/Matcher/AnyOfMatcher.php
+++ src/Matcher/AnyOfMatcher.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace EspressoWebDriver\Matcher;
 
 use EspressoWebDriver\Core\EspressoOptions;
+use EspressoWebDriver\Traits\HasAutomaticWait;
 use Facebook\WebDriver\WebDriverElement;
 
 final readonly class AnyOfMatcher implements MatcherInterface
 {
+    use HasAutomaticWait;
+
     /**
      * @var MatcherInterface[]
      */
@@ -21,10 +24,28 @@ final readonly class AnyOfMatcher implements MatcherInterface
 
     public function match(WebDriverElement $root, EspressoOptions $options): array
     {
+        return $this->wait(
+            $options->waitTimeoutInSeconds,
+            $options->waitIntervalInMilliseconds,
+            fn () => $this->findElements($root),
+        );
+    }
+
+    /**
+     * @return WebDriverElement[]
+     */
+    private function findElements(WebDriverElement $root): array
+    {
         $elementsByMatcher = [];
 
+        // Since we are waiting ourselves, we don't want the child matchers to wait as well.
+        $instantOptions = new EspressoOptions(
+            waitTimeoutInSeconds: 0,
+            waitIntervalInMilliseconds: 0,
+        );
+
         foreach ($this->matchers as $matcher) {
-            $elementsByMatcher[] = $matcher->match($root, $options);
+            $elementsByMatcher[] = $matcher->match($root, $instantOptions);
         }
 
         return array_merge(...$elementsByMatcher);
