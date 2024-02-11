a4be5c813f42d469a15d05b3d6b02257696cc84e
Erik Perri <erik.perri@gmail.com>
2024-02-11 13:44:37 -0500

    test: Added isDisplayed feature tests

---
+++ tests/Feature/Matcher/IsDisplayedMatcherFeatureTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
+
+namespace EspressoWebDriver\Tests\Feature\Matcher;
+
+use EspressoWebDriver\Core\EspressoOptions;
+use EspressoWebDriver\Core\PhpunitReporter;
+use EspressoWebDriver\Matcher\IsDisplayedMatcher;
+use EspressoWebDriver\Tests\Feature\BaseFeatureTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversFunction;
+
+use function EspressoWebDriver\click;
+use function EspressoWebDriver\doesNotMatch;
+use function EspressoWebDriver\isDisplayed;
+use function EspressoWebDriver\matches;
+use function EspressoWebDriver\scrollTo;
+use function EspressoWebDriver\withDriver;
+use function EspressoWebDriver\withText;
+
+#[CoversClass(IsDisplayedMatcher::class)]
+#[CoversFunction('EspressoWebDriver\isDisplayed')]
+class IsDisplayedMatcherFeatureTest extends BaseFeatureTestCase
+{
+    public function testCannotSeeElementsThatAreOutOfTheViewport(): void
+    {
+        // Arrange
+        $driver = $this->driver()->get($this->mockStaticUrl('matchers/is-displayed.html'));
+
+        $options = new EspressoOptions(
+            waitTimeoutInSeconds: 0,
+            assertionReporter: new PhpunitReporter,
+        );
+
+        $espresso = withDriver($driver, $options);
+
+        $hiddenUntilScrollElement = $espresso->onElement(withText('Mock X'));
+
+        // Act and Assert
+        $hiddenUntilScrollElement
+            ->check(doesNotMatch(isDisplayed()))
+            ->perform(scrollTo())
+            ->check(matches(isDisplayed()));
+    }
+
+    public function testCannotSeeElementsThatAreHiddenWithCss(): void
+    {
+        // Arrange
+        $driver = $this->driver()->get($this->mockStaticUrl('matchers/is-displayed.html'));
+
+        $options = new EspressoOptions(
+            waitTimeoutInSeconds: 0,
+            assertionReporter: new PhpunitReporter,
+        );
+
+        $espresso = withDriver($driver, $options);
+
+        $targetElement = $espresso->onElement(withText('Mock C'));
+        $hiddenUntilClickElement = $espresso->onElement(withText('Hidden'));
+
+        // Act and Assert
+        $hiddenUntilClickElement->check(doesNotMatch(isDisplayed()));
+
+        $targetElement->perform(click());
+
+        $hiddenUntilClickElement->check(matches(isDisplayed()));
+    }
+}
