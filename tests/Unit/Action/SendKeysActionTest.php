ad5501250b54bf45659304b00d07d095fdaf1872
Erik Perri <erik.perri@gmail.com>
2024-02-13 20:13:37 -0500

    test: Added check of throw path

--- tests/Unit/Action/SendKeysActionTest.php
+++ tests/Unit/Action/SendKeysActionTest.php
@@ -7,7 +7,12 @@ declare(strict_types=1);
 namespace Action;
 
 use EspressoWebDriver\Action\SendKeysAction;
+use EspressoWebDriver\Core\EspressoContext;
+use EspressoWebDriver\Core\EspressoOptions;
+use EspressoWebDriver\Exception\PerformException;
 use EspressoWebDriver\Tests\Unit\BaseUnitTestCase;
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverElement;
 use Facebook\WebDriver\WebDriverKeys;
 use PHPUnit\Framework\Attributes\CoversClass;
 
@@ -36,5 +41,29 @@ class SendKeysActionTest extends BaseUnitTestCase
 
         // Assert
         $this->assertSame('sendKeys(use, type, text)', $result);
+    }
+
+    public function testThrowsIfDriverDoesNotHaveInputAccess(): void
+    {
+        // Expectations
+        $this->expectException(PerformException::class);
+        $this->expectExceptionMessage(
+            'Failed to perform action sendKeys(), driver does not have access to input devices',
+        );
+
+        // Arrange
+        $assertion = new SendKeysAction();
+
+        $element = $this->createMock(WebDriverElement::class);
+        $context = new EspressoContext(
+            driver: $this->createMock(WebDriver::class),
+            options: new EspressoOptions(),
+        );
+
+        // Act
+        $assertion->perform($element, $context);
+
+        // Assert
+        // No assertions, only expectations.
     }
 }
