2d42c77e57e3213eebe1bbbeec284f56cf3e2ca0
Erik Perri <erik.perri@gmail.com>
2024-02-14 20:17:24 -0500

    refactor: Removed no longer needed try/catch

--- src/Matcher/NotMatcher.php
+++ src/Matcher/NotMatcher.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace EspressoWebDriver\Matcher;
 
-use EspressoWebDriver\Exception\NoMatchingElementException;
-
 final readonly class NotMatcher implements MatcherInterface
 {
     public function __construct(private MatcherInterface $matcher)
@@ -15,15 +13,11 @@ final readonly class NotMatcher implements MatcherInterface
 
     public function match(MatchResult $container, MatchContext $context): MatchResult
     {
-        try {
-            return $this->matcher->match($container, new MatchContext(
-                driver: $context->driver,
-                isNegated: true,
-                options: $context->options,
-            ));
-        } catch (NoMatchingElementException) {
-            return new MatchResult(matcher: $this, result: []);
-        }
+        return $this->matcher->match($container, new MatchContext(
+            driver: $context->driver,
+            isNegated: true,
+            options: $context->options,
+        ));
     }
 
     public function __toString(): string
