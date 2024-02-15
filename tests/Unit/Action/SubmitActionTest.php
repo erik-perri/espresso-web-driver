ae1b2f3545edf3ad6f2774edcf2579a9a95c7574
Erik Perri <erik.perri@gmail.com>
2024-02-15 13:01:48 -0500

    test: Added submit element check test

--- tests/Unit/Action/SubmitActionTest.php
+++ tests/Unit/Action/SubmitActionTest.php
@@ -7,7 +7,12 @@ declare(strict_types=1);
 namespace Action;
 
 use EspressoWebDriver\Action\SubmitAction;
+use EspressoWebDriver\Core\EspressoContext;
+use EspressoWebDriver\Core\EspressoOptions;
+use EspressoWebDriver\Exception\PerformException;
 use EspressoWebDriver\Tests\Unit\BaseUnitTestCase;
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverElement;
 use PHPUnit\Framework\Attributes\CoversClass;
 
 #[CoversClass(SubmitAction::class)]
@@ -23,5 +28,29 @@ class SubmitActionTest extends BaseUnitTestCase
 
         // Assert
         $this->assertSame('submit', $result);
+    }
+
+    public function testThrowsPerformExceptionWhenProvidedElementUnrelatedToForm(): void
+    {
+        // Expectations
+        $this->expectException(PerformException::class);
+        $this->expectExceptionMessage('Failed to perform action submit on <mock>, not a form related element');
+
+        // Arrange
+        $assertion = new SubmitAction();
+
+        $mockElement = $this->createMock(WebDriverElement::class);
+        $mockElement->method('getTagName')->willReturn('mock');
+
+        $mockContext = new EspressoContext(
+            driver: $this->createMock(WebDriver::class),
+            options: new EspressoOptions(waitTimeoutInSeconds: 0),
+        );
+
+        // Act
+        $assertion->perform($mockElement, $mockContext);
+
+        // Assert
+        // No assertions, only expectations.
     }
 }
