ae4c8b7c14bb0b31d03ad6245c677fdaddb5e736
Erik Perri <erik.perri@gmail.com>
2024-02-15 12:59:27 -0500

    test: Added scrollTo driver check test

--- tests/Unit/Action/ScrollToActionTest.php
+++ tests/Unit/Action/ScrollToActionTest.php
@@ -7,7 +7,12 @@ declare(strict_types=1);
 namespace Action;
 
 use EspressoWebDriver\Action\ScrollToAction;
+use EspressoWebDriver\Core\EspressoContext;
+use EspressoWebDriver\Core\EspressoOptions;
+use EspressoWebDriver\Exception\PerformException;
 use EspressoWebDriver\Tests\Unit\BaseUnitTestCase;
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverElement;
 use PHPUnit\Framework\Attributes\CoversClass;
 
 #[CoversClass(ScrollToAction::class)]
@@ -23,5 +28,29 @@ class ScrollToActionTest extends BaseUnitTestCase
 
         // Assert
         $this->assertSame('scrollTo', $result);
+    }
+
+    public function testThrowsPerformExceptionWhenMissingExecuteAccess(): void
+    {
+        // Expectations
+        $this->expectException(PerformException::class);
+        $this->expectExceptionMessage('Failed to perform action scrollTo on <mock>, driver does not have access to executeScript');
+
+        // Arrange
+        $assertion = new ScrollToAction();
+
+        $mockElement = $this->createMock(WebDriverElement::class);
+        $mockElement->method('getTagName')->willReturn('mock');
+
+        $mockContext = new EspressoContext(
+            driver: $this->createMock(WebDriver::class),
+            options: new EspressoOptions(waitTimeoutInSeconds: 0),
+        );
+
+        // Act
+        $assertion->perform($mockElement, $mockContext);
+
+        // Assert
+        // No assertions, only expectations.
     }
 }
