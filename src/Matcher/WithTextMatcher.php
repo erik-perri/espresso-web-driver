27af3a2dace595e6f2810db24f7b99c18cde41b0
Erik Perri <erik.perri@gmail.com>
2024-03-11 22:14:00 -0400

    fix: Fixed with text using normalize for Xpath but not input

--- src/Matcher/WithTextMatcher.php
+++ src/Matcher/WithTextMatcher.php
@@ -6,16 +6,20 @@ namespace EspressoWebDriver\Matcher;
 
 use EspressoWebDriver\Core\EspressoContext;
 use EspressoWebDriver\Core\MatchResult;
+use EspressoWebDriver\Utilities\TextNormalizer;
 use EspressoWebDriver\Utilities\XPathStringWrapper;
 use Facebook\WebDriver\WebDriverBy;
 
 final readonly class WithTextMatcher implements MatcherInterface, NegativeMatcherInterface
 {
+    private string $normalizedText;
+
     private string $wrappedText;
 
-    public function __construct(private string $text)
+    public function __construct(string $text)
     {
-        $this->wrappedText = (new XPathStringWrapper)->wrap($this->text);
+        $this->normalizedText = (new TextNormalizer)->normalize($text);
+        $this->wrappedText = (new XPathStringWrapper)->wrap($this->normalizedText);
     }
 
     public function match(MatchResult $container, EspressoContext $context): array
@@ -40,6 +44,6 @@ final readonly class WithTextMatcher implements MatcherInterface, NegativeMatche
 
     public function __toString(): string
     {
-        return sprintf('withText(%1$s)', $this->text);
+        return sprintf('withText(%1$s)', $this->normalizedText);
     }
 }
