e34ed97434d7179acd2fece7111a53fd0ec5fb8c
Erik Perri <erik.perri@gmail.com>
2024-03-13 22:56:46 -0400

    refactor: Added ElementNotInteractableException to click animation retry exceptions

--- src/Action/ClickAction.php
+++ src/Action/ClickAction.php
@@ -7,23 +7,27 @@ namespace EspressoWebDriver\Action;
 use EspressoWebDriver\Core\EspressoContext;
 use EspressoWebDriver\Core\MatchResult;
 use Facebook\WebDriver\Exception\ElementClickInterceptedException;
+use Facebook\WebDriver\Exception\ElementNotInteractableException;
 
 final readonly class ClickAction implements ActionInterface
 {
+    /**
+     * @noinspection PhpRedundantCatchClauseInspection
+     */
     public function perform(MatchResult $target, EspressoContext $context): bool
     {
         $interactableParent = $context->options->elementLocator
             ->findNonScreenReaderParent($target->single(), $target->container?->single());
 
         // TODO Build retry logic in a configurable way
-        $waitIntervalInMicroseconds = 250 * 1000;
+        $animationWaitIntervalInMicroseconds = 250 * 1000;
 
         try {
             $interactableParent->click();
 
             return true;
-        } /** @noinspection PhpRedundantCatchClauseInspection */ catch (ElementClickInterceptedException $e) {
-            usleep($waitIntervalInMicroseconds);
+        } catch (ElementClickInterceptedException|ElementNotInteractableException) {
+            usleep($animationWaitIntervalInMicroseconds);
 
             $interactableParent->click();
         }
